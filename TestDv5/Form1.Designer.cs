88f74783dee6371f89e83d628a77fee5d7b1a9d4
Bogatyrev.P <Bogatyrev.P@docsvision.com>
2021-12-27 10:16:20 +0300

    Intial.

---
+++ TestDv5/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TestDv5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelRPS = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(15, 47);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(171, 23);
+            this.buttonStart.TabIndex = 12;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAddress.Location = new System.Drawing.Point(85, 12);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(307, 20);
+            this.textBoxAddress.TabIndex = 35;
+            this.textBoxAddress.Text = "http://localhost/testsite";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 15);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "Reqeusts per second:";
+            // 
+            // labelRPS
+            // 
+            this.labelRPS.AutoSize = true;
+            this.labelRPS.Location = new System.Drawing.Point(148, 90);
+            this.labelRPS.Name = "labelRPS";
+            this.labelRPS.Size = new System.Drawing.Size(14, 15);
+            this.labelRPS.TabIndex = 41;
+            this.labelRPS.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 15);
+            this.label6.TabIndex = 50;
+            this.label6.Text = "URL:";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(196, 47);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(198, 23);
+            this.buttonStop.TabIndex = 51;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 131);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.labelRPS);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.buttonStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "S";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelRPS;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ TestDv5/Form1.cs
@@ -0,0 +1,65 @@
+﻿using DocsVision.BackOffice.CardLib.CardDefs;
+using DocsVision.BackOffice.ObjectModel;
+using DocsVision.BackOffice.ObjectModel.Services;
+using DocsVision.DocumentsManagement.ObjectModel.Services;
+using DocsVision.Platform.ObjectManager;
+using DocsVision.Platform.ObjectManager.Rest;
+using DocsVision.Platform.ObjectManager.SystemCards;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestDv5
+{
+    public partial class Form1 : Form
+    {
+        bool working = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonStart_Click(object sender, EventArgs e)
+        {
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.UseDefaultCredentials = true;
+            var httpClient = new HttpClient(httpClientHandler);
+            var from = DateTime.Now;
+            var countPerSecond = 0;
+            working = true;
+            while (working)
+            {
+                var response = await httpClient.GetAsync(textBoxAddress.Text);
+                var content = await response.Content.ReadAsStringAsync();
+                countPerSecond++;
+                if ((DateTime.Now - from).TotalSeconds >= 1)
+                {
+                    this.labelRPS.Text = countPerSecond.ToString();
+                    countPerSecond = 0;
+                    from = DateTime.Now;
+                }
+                Application.DoEvents();
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            working = false;
+        }
+    }
+}
+
